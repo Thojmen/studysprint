b13fbfa98370791664141b958c30766942af02c8
Thojmen <thijmenvanbreugel@gmail.com>
2026-06-18 13:45:53 +0200

    vond net een kleine foute met het maken van een task nu opgelost

--- create-task.php
+++ create-task.php
@@ -5,29 +5,34 @@ include 'includes/db.php';
 
 $project_id = $_GET['project'];
 
-if(isset($_POST['save']))
+if(isset($_POST['create']))
 {
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $assigned_to = $_POST['assigned_to'];
+
     $stmt = $pdo->prepare("
-    INSERT INTO tasks
-    (
-        project_id,
-        title,
-        description,
-        assigned_to,
-        status
-    )
-    VALUES(?,?,?,?,?)
+        INSERT INTO tasks
+        (
+            project_id,
+            title,
+            description,
+            assigned_to,
+            status
+        )
+        VALUES (?, ?, ?, ?, ?)
     ");
 
     $stmt->execute([
         $project_id,
-        $_POST['title'],
-        $_POST['description'],
-        $_POST['assigned_to'],
+        $title,
+        $description,
+        $assigned_to,
         'todo'
     ]);
 
     header("Location: kanban.php?id=$project_id");
+    exit;
 }
 ?>
 <!DOCTYPE html>
@@ -41,41 +46,41 @@ if(isset($_POST['save']))
 <body>
 
 <?php include 'includes/sidebar.php'; ?>
-    <div class="content">
+<div class="content">
 
-        <a
-            class="back-button"
-            href="kanban.php?id=<?= $project_id ?>">
-            ← Terug naar Kanban
-        </a>
+    <a
+        class="back-button"
+        href="kanban.php?id=<?= $project_id ?>">
+        ← Terug naar Kanban
+    </a>
 
-        <h1>Nieuwe Taak</h1>
+    <h1>Nieuwe Taak</h1>
 
-        <form method="POST" class="task-form">
+    <form method="POST" class="task-form">
 
-            <label>Titel</label>
-            <input
-                type="text"
-                name="title"
-                required>
+        <label>Titel</label>
+        <input
+            type="text"
+            name="title"
+            required>
 
-            <label>Omschrijving</label>
-            <textarea
-                name="description"></textarea>
+        <label>Omschrijving</label>
+        <textarea
+            name="description"></textarea>
 
-            <label>Uitvoerder</label>
-            <input
-                type="text"
-                name="assigned_to">
+        <label>Uitvoerder</label>
+        <input
+            type="text"
+            name="assigned_to">
 
-            <button
-                type="submit"
-                name="create">
-                Taak Opslaan
-            </button>
+        <button
+            type="submit"
+            name="create">
+            Taak Opslaan
+        </button>
 
-        </form>
+    </form>
 
-    </div>
+</div>
 </body>
 </html>
